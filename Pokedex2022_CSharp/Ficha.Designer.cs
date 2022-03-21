fc9e4bfe26f2c7f25b927b96b0b3b14f78cfb3dc
xp <xp@CETYSDAM00>
2022-03-21 16:29:00 +0100

    Agregar archivos de proyecto.

---
+++ Pokedex2022_CSharp/Ficha.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Pokedex2022_CSharp
+{
+    partial class Ficha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nombrePokemon = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nombrePokemon
+            // 
+            this.nombrePokemon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.nombrePokemon.Location = new System.Drawing.Point(17, 18);
+            this.nombrePokemon.Name = "nombrePokemon";
+            this.nombrePokemon.Size = new System.Drawing.Size(201, 13);
+            this.nombrePokemon.TabIndex = 0;
+            this.nombrePokemon.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Ficha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Pokedex2022_CSharp.Properties.Resources.PokedexFicha;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nombrePokemon);
+            this.Name = "Ficha";
+            this.Text = "Ficha";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nombrePokemon;
+    }
+}
---
+++ Pokedex2022_CSharp/Ficha.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pokedex2022_CSharp
+{
+    public partial class Ficha : Form
+    {
+        Conexion miConexion = new Conexion();
+        DataTable misPokemons = new DataTable();
+        int idActual = 1; //guarda el id del pokemon que se está viendo
+        public Ficha()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            idActual--;
+            misPokemons = miConexion.getPokemonPorId(idActual);
+            nombrePokemon.Text = misPokemons.Rows[0]["nombre"].ToString();
+        }
+    }
+}
